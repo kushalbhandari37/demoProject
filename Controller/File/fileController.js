ec10db9861b892786c27bba44032fec2160da947
Kushal <koolbutdeadly.kushal08@gmail.com>
2022-01-17 16:19:34 +0545

    Added file Read functionality

--- Controller/File/fileController.js
+++ Controller/File/fileController.js
@@ -1,8 +1,10 @@
 const fs = require('fs');
 const path = require('path');
 const uploadPath = path.join('./uploads');
+const csv = require('csv-parser')
 
 module.exports = {
+    //upload file
     uploadFile: async (req,res) => {        
         req.pipe(req.busboy)    //piping request         
         req.busboy.on('file',(fieldname, file, filename)=>{
@@ -17,5 +19,22 @@ module.exports = {
                 res.status(200).json(`${filename.filename} uploaded successfully.`);
             });
         }) 
+    },
+
+    //Read CSV file and send data as a response
+    readFile: async (req,res) => {  
+        let result = [];     
+        //Create a read stream 
+        fs.createReadStream(path.join('./uploads/data.csv'))
+        .pipe(csv())
+        .on('data',(data)=>{ 
+            //pushing csv data to result
+            result.push(data);
+        })
+        .on('end',()=>{
+            JSON.stringify(result);
+            return res.json({message:"File Read Successfully. This is CSV Data in JSON Format",result});
+        });
     }
+    
 }
--- routes/userRoute/userRoute.js
+++ routes/userRoute/userRoute.js
@@ -1,10 +1,11 @@
 const userController = require('../../Controller/User/userController');
-const uploadController = require('../../Controller/Upload/uploadController');
+const fileController = require('../../Controller/File/fileController');
 const registrationSchema = require('../../Validations/registrationSchema');
 const validationRequest = require('../../middleware/validation');
 const { route } = require('express/lib/application');
 
 module.exports = (router) => {    
     router.post('/register',registrationSchema,validationRequest, userController.registerUser);
-    router.post('/upload',uploadController.uploadFile);
+    router.post('/upload',fileController.uploadFile);
+    router.get('/read',fileController.readFile);
 }
